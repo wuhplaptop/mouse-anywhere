946f5757bf1befcf12dae813ba0795a4c87f84eb
wuhplaptop <wuhplaptop@gmail.com>
2024-12-24 01:18:54 -0500

    Create __init__.py

---
+++ mouse_anywhere/__init__.py
@@ -0,0 +1,10 @@
+# mouse_anywhere/__init__.py
+from .mouse_anywhere import (
+    initialize,
+    shutdown,
+    set_cursor_abs,
+    set_cursor_rel,
+    enqueue_target_abs,
+    enqueue_target_rel,
+    MouseAnywhere
+)
