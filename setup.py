3adb9d555123cd5334beaa6053be4f6a34e37d20
wuhplaptop <wuhplaptop@gmail.com>
2024-12-24 05:21:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mouse-anywhere',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python package for smooth mouse movement using a C library.',
     long_description=long_description,
     long_description_content_type="text/markdown",
