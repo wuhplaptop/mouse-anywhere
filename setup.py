8a719873fe4144efb13de4a9587d86a067ab4431
wuhplaptop <wuhplaptop@gmail.com>
2024-12-24 01:24:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@
 from setuptools import setup, find_packages
 import os
 
+# Read the README for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='mouse-anywhere',
     version='0.1.0',
     description='A Python package for smooth mouse movement using a C library.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='wuhp',
     author_email='wuhp@gmail.com',
-    url='https://github.com/yourusername/mouse-anywhere',  # Replace with your repository URL
+    url='https://github.com/wuhplaptop/mouse-anywhere',  # Replace with your repository URL
     packages=find_packages(),
     include_package_data=True,
     package_data={
@@ -17,6 +23,14 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: Microsoft :: Windows',
+        'License :: OSI Approved :: MIT License',  # Adjust license as needed
     ],
     python_requires='>=3.6',
+    install_requires=[
+        # List any dependencies if required
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/wuhplaptop/mouse-anywhere/issues',
+        'Source': 'https://github.com/wuhplaptop/mouse-anywhere',
+    },
 )
