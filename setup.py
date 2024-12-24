4681ced8c0d3a062fea2605883facf114cce0919
wuhplaptop <wuhplaptop@gmail.com>
2024-12-24 00:28:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+# setup.py
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py as build_py_orig
+import os
+
+class BuildPy(build_py_orig):
+    def run(self):
+        # Ensure the DLL is included in the package
+        build_py_orig.run(self)
+        dll_source = 'mouse-anywhere.dll'
+        dll_dest = os.path.join(self.build_lib, 'mouse_anywhere', 'mouse-anywhere.dll')
+        self.copy_file(dll_source, dll_dest)
 
 setup(
     name='mouse-anywhere',
@@ -6,12 +17,13 @@ setup(
     description='A Python package for smooth mouse movement using a C library.',
     author='wuhp',
     author_email='wuhp@gmail.com',
-    url='https://github.com/yourusername/mouse-anywhere',
+    url='https://github.com/yourusername/mouse-anywhere',  # Replace with your repository URL
     packages=find_packages(),
     include_package_data=True,
     package_data={
         'mouse_anywhere': ['mouse-anywhere.dll'],
     },
+    cmdclass={'build_py': BuildPy},
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: Microsoft :: Windows',
